de029e78babfad9f57c71070ca620abff978d799
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2021-09-25 21:49:52 -0500

    feat: trait for string was added

--- src/std_library/string.rs
+++ src/std_library/string.rs
@@ -1,6 +1,4 @@
-trait VectorString {
-    fn join_to_string(&self) -> String;
-}
+trait VectorString { fn join_to_string(&self) -> String; }
 impl VectorString for Vec<u8> {
     fn join_to_string(&self) -> String {
         return self.iter().map(|&x| (x as char).to_string()).collect::<String>();
@@ -11,11 +9,26 @@ impl VectorString for Vec<char> {
         return self.iter().map(|x| x.to_string()).collect::<String>();
     }
 }
-trait StringVec {
-    fn to_vector(&self) -> Vec<char>;
-}
-impl StringVec for String {
-    fn to_vector(&self) -> Vec<char> {
+trait ToChars { fn to_chars(&self) -> Vec<char>; }
+trait ToBytes { fn to_bytes(&self) -> Vec<u8>; }
+impl ToChars for String {
+    fn to_chars(&self) -> Vec<char> {
         return self.chars().collect::<Vec<_>>();
+    }
+}
+impl ToBytes for String {
+    fn to_bytes(&self) -> Vec<u8> {
+        return self.chars().map(|ch| (ch as u8)).collect::<Vec<_>>();
+    }
+}
+impl ToBytes for Vec<char> {
+    fn to_bytes(&self) -> Vec<u8> {
+        return self.iter().map(|&ch| (ch as u8)).collect::<Vec<_>>();
+    }
+}
+trait ToByte { fn at(&self, index: usize) -> u8; }
+impl ToByte for Vec<char> {
+    fn at(&self, index: usize) -> u8 {
+        return self[index] as u8;
     }
 }
